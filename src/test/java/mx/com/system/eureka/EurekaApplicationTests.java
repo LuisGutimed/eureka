312ab68b474bdeb3f38e205105fbf771d81b3c19
Luis Gutierrez <lgutierrez.stgt@gmail.com>
2025-02-13 18:59:13 -0600

    init: eureka server, discovery

---
+++ src/test/java/mx/com/system/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.system.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
